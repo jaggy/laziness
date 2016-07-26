e302c6bb9695164adfb7a3ff94b429d16ca6cefb
Jaggy Gauran <i.am@jag.gy>
2016-07-26 13:47:14 +0800

    Use the home folder to look for a .workrc file.

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ require __DIR__ . '/vendor/autoload.php';
 use Dotenv\Dotenv;
 use Illuminate\Filesystem\Filesystem;
 
-(new Dotenv(__DIR__))->load();
+(new Dotenv(getenv('HOME'), '.workrc'))->load();
 
 Filesystem::macro('touch', function ($filename) {
     $this->append($filename, '');
